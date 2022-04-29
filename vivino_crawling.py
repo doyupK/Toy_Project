1c8ea2523cfbb6b50a3619b59dde3c49b927000a
minsu86 <manager.kim86@gmail.com>
2022-04-29 13:38:52 +0900

    4/29 13:32 민수
    < 작업 내용 >
    - 전체 페이지 링크 연결 작업
    - 사이트별 추천 와인 게시판 추가 winelist.html
    - 게시판 페이지네이션 추가
    - 필요없는 페이지 삭제
    - Crawling_detail.html 이름 변경 C -> c
    
    < 작업 페이지 >
    app.py / vivino_crawling.py
    
    crawling_detail.html / detail.html / editprofile.html /
    index.html / intro2.html / login.html / signup.html /
    winelist.html / winenot.html
    
    style.css / signup.js

--- vivino_crawling.py
+++ vivino_crawling.py
@@ -10,28 +10,33 @@ import time
 
 ca = certifi.where()
 # client = MongoClient('mongodb+srv://test:sparta@sparta.eacl0.mongodb.net/sparta?retryWrites=true&w=majority', tlsCAFile=ca) #이동재
-client = MongoClient('mongodb+srv://test:sparta@cluster0.kxazb.mongodb.net/Cluster0?retryWrites=true&w=majority', tlsCAFile=ca) #DY
+client = MongoClient('mongodb+srv://test:sparta@cluster0.kxazb.mongodb.net/Cluster0?retryWrites=true&w=majority',
+                     tlsCAFile=ca)  # DY
 db = client.dbsparta
 
 db.vivino_wines.delete_many({})
 db.vivino_wines_list.delete_many({})
 
 chrome_options = webdriver.ChromeOptions()
+
 chrome_options.add_argument('headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('window-size=1920x1080')
-chrome_options.add_argument("user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+
+chrome_options.add_argument(
+    "user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36")
+# chrome_options.add_argument(
+#     "user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
+
 driver = webdriver.Chrome(options=chrome_options)
-
-
 # 추천 리스트 크롤링
 driver.get('https://www.vivino.com/')
-
 WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "toplistsBand")))
 
 for i in range(10):
     try:
-        i = driver.find_element(by=By.XPATH, value='/html/body/div[2]/div[4]/div/div/div[2]/div/div/div/div[2]/div/span[1]/div')
+        i = driver.find_element(by=By.XPATH,
+                                value='/html/body/div[2]/div[4]/div/div/div[2]/div/div/div/div[2]/div/span[1]/div')
         i.click()
         time.sleep(0.2)
     except NoSuchElementException:
@@ -44,10 +49,14 @@ count = 0
 for wine in wines:
     # 고유번호(Key) 생성
     count = count + 1
-    wine_image = wine.select_one('div > a > div > div.wineCard__bottleSection--3Bzic > img')['src'][2:]
-    wine_name = wine.select_one('div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfo__vintage--2wqwE > div.wineInfoVintage__vintage--VvWlU.wineInfoVintage__truncate--3QAtw').text
-    wine_maker = wine.select_one('div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfo__vintage--2wqwE > div:nth-child(1)').text
-    wine_region = wine.select_one('div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoLocation__wineInfoLocation--BmkcO > div').text
+    image = wine.select_one('div > a > div > div.wineCard__bottleSection--3Bzic > img')['src'][2:]
+    wine_image = "https://" + image
+    wine_name = wine.select_one(
+        'div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfo__vintage--2wqwE > div.wineInfoVintage__vintage--VvWlU.wineInfoVintage__truncate--3QAtw').text
+    wine_maker = wine.select_one(
+        'div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfo__vintage--2wqwE > div:nth-child(1)').text
+    wine_region = wine.select_one(
+        'div > a > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoLocation__wineInfoLocation--BmkcO > div').text
 
     doc = {
         'post_num': count,
@@ -58,12 +67,12 @@ for wine in wines:
     }
     db.vivino_wines.insert_one(doc)
 
-
 # 와인 리스트 페이지 크롤링
 
 SCROLL_PAUSE_TIME = 1
 
-driver.get('https://www.vivino.com/explore?e=eJzLLbI11rNQy83MszU0AAK13MQKWxMwK7nS1jtILRlIhKsV2BqqpafZliUWZaaWJOao5Rel2KrlJ1XaqpWXRMcCJcGUEQCDaRfR/')
+driver.get(
+    'https://www.vivino.com/explore?e=eJzLLbI11rNQy83MszU0AAK13MQKWxMwK7nS1jtILRlIhKsV2BqqpafZliUWZaaWJOao5Rel2KrlJ1XaqpWXRMcCJcGUEQCDaRfR/')
 #
 driver.implicitly_wait(5)
 # 초기 창 높이 값
@@ -96,15 +105,20 @@ while 1:
 
 soup = BeautifulSoup(driver.page_source, 'html.parser')
 
-posts = soup.select('#explore-page-app > div > div > div.explorerPage__columns--1TTaK > div.explorerPage__results--3wqLw > div > div')
+posts = soup.select(
+    '#explore-page-app > div > div > div.explorerPage__columns--1TTaK > div.explorerPage__results--3wqLw > div > div')
 count = 0
 for post in posts:
     # 고유 번호(Key)
     count = count + 1
-    wine_image = post.select_one('div > a > div > div > img')['src'][2:]
-    wine_name = post.select_one('div > a > div > div.wineCard__infoColumn--3NKrN > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfoVintage__large--OaWjm.wineInfo__vintage--2wqwE > div.wineInfoVintage__vintage--VvWlU.wineInfoVintage__truncate--3QAtw').text
-    wine_maker = post.select_one('div > a > div > div.wineCard__infoColumn--3NKrN > div > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfoVintage__large--OaWjm.wineInfo__vintage--2wqwE > div').text
-    wine_region = post.select_one('div > a > div > div.wineCard__infoColumn--3NKrN > div > div.wineInfoLocation__wineInfoLocation--BmkcO > div').text
+    image = post.select_one('div > a > div > div > img')['src'][2:]
+    wine_image = "https://" + image
+    wine_name = post.select_one(
+        'div > a > div > div.wineCard__infoColumn--3NKrN > div.wineInfo__wineInfo--Sx0T0 > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfoVintage__large--OaWjm.wineInfo__vintage--2wqwE > div.wineInfoVintage__vintage--VvWlU.wineInfoVintage__truncate--3QAtw').text
+    wine_maker = post.select_one(
+        'div > a > div > div.wineCard__infoColumn--3NKrN > div > div.wineInfoVintage__wineInfoVintage--bXr7s.wineInfoVintage__large--OaWjm.wineInfo__vintage--2wqwE > div').text
+    wine_region = post.select_one(
+        'div > a > div > div.wineCard__infoColumn--3NKrN > div > div.wineInfoLocation__wineInfoLocation--BmkcO > div').text
 
     doc = {
         'post_num': count,
@@ -115,6 +129,4 @@ for post in posts:
     }
     db.vivino_wines_list.insert_one(doc)
 
-
 driver.quit()
-
